e8e8c1f4bca75a28ad59db032c966bb591dc057f
lalith050101 <yvlalithkumar@gmail.com>
2021-07-24 18:50:34 +0530

    updated app.js

--- js/app.js
+++ js/app.js
@@ -10,28 +10,3 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
-
-// let deferredPrompt;
-// window.addEventListener("beforeinstallprompt", (e) => {
-//   console.log("inside beforeinstallprompt");
-//   //prevent chrome 67 or earlier from automatically showing prompt
-//   e.preventDefault();
-//   //stash the event so it can be triggered later
-//   deferredPrompt = e;
-//   //updated UI notify the user they can add to home screen
-//   btnAdd.style.display = "block";
-// });
-
-// btnAdd.addEventListener("click", (e) => {
-//   deferredPrompt.prompt();
-//   deferredPrompt.userChoice.then((choiceResult) => {
-//     if (choiceResult.outcome === "accepted") {
-//       console.log("User accepted add to HS prompt");
-//     }
-//     deferredPrompt = null;
-//   });
-// });
-
-// window.addEventListener("appinstalled", (evt) => {
-//   app.logEvent("a2hs", "installed");
-// });
