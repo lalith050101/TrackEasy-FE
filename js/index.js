303cef20a6166fc1e2477c4c227eeb1dcf6bdb7c
lalith050101 <yvlalithkumar@gmail.com>
2021-07-24 21:56:47 +0530

    updated image styling and error display

--- js/index.js
+++ js/index.js
@@ -41,19 +41,23 @@ function loginfn(e) {
       if (res.ok) {
         console.log("success");
         return res.json();
-      } else {
-        console.log("not successful");
+      } else if (res.status === 401) {
+        console.log("wrong creds");
+        document.getElementById("wrongCredentials").style.visibility =
+          "visible";
+
+        return Promise.reject(res);
       }
+      return Promise.reject(res);
     })
     .then((data) => {
-      //console.log(data);
       localStorage.setItem("userstatus", JSON.stringify(data));
-      //console.log("here");
+
       var x = JSON.parse(localStorage.getItem("userstatus"));
       console.log(x.role);
       if (x.role === "admin") window.location = "AdminHomePage.html";
       else if (x.role === "student") window.location = "StudentHomePage.html";
       else if (x.role === "company") window.location = "CompanyHomePage.html";
     })
-    .catch((error) => console.log("ERROR" + error));
+    .catch((error) => console.log("can't able to login"));
 }
