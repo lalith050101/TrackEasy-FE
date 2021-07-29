7354a987f716f22945900613e296b7116888eda6
lalith050101 <yvlalithkumar@gmail.com>
2021-07-29 16:01:09 +0530

    updated firebase version

--- sw.js
+++ sw.js
@@ -2,11 +2,9 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-importScripts("https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 
-importScripts(
-  "https://www.gstatic.com/firebasejs/7.10.0/firebase-messaging.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDWdA0CLOO17kPItEDjL0-WXDzxCuRULLE",
