49202078dcd96c2187051d7e9f50953d5538d946
lalith050101 <yvlalithkumar@gmail.com>
2021-08-02 10:56:33 +0530

    updated notifications redirect

--- js/StudentNotifications.js
+++ js/StudentNotifications.js
@@ -17,6 +17,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
       }
     })
     .then((data) => {
+      if (data.length === 0) {
+        document.getElementById("message").innerHTML = "<br>No Notifications";
+      }
       if (data.length > 0) {
         var temp = "";
 
@@ -24,12 +27,17 @@ document.addEventListener("DOMContentLoaded", (e) => {
           temp += "<tr>";
           temp += "<td>" + notification.senderName + "</td>";
           temp += "<td>" + notification.title + "</td>";
-          temp += "<td>" + notification.description + "</td></tr>";
+          temp += "<td>" + notification.description + "</td>";
 
-          //   temp += `<td><button class="view"  onclick='viewJobDetails(${job.jobId})'><div class="i"><i class="fas fa-sign-in-alt"></i></div></button></td></tr>`;
+          temp += `<td><button class="view"  onclick='viewJobDetails(${notification.jobId})'><div class="i"><i class="fas fa-sign-in-alt"></i></div></button></td></tr>`;
         });
         document.getElementById("notifications").innerHTML = temp;
       }
     })
     .catch((error) => console.log("ERROR" + error));
 });
+
+function viewJobDetails(jobId) {
+  console.log("jobid new");
+  window.location = "JobDetails.html?jobId=" + jobId;
+}
